395949c229650b7fe753a5953621cee7ea6f8855
Will Shu <will@Wills-iMac.local>
2018-11-13 21:07:13 +0800

    dev modified postRoutes

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -4,30 +4,40 @@ const mongoose = require('mongoose');
 const Post = mongoose.model('post');
 
 module.exports = app => {
-    // get all posts
+    // get a certain amount of recent posts
     app.get('/posts', async (req, res) => {
-        const posts = await Post.find({});
+        const { page, count } = req.body;
+        const posts = await Post.find({})
+                .skip(Post.count() - count*page)
+                .limit(count);
         res.send(posts);
     });
     // write a post
     app.post('/posts', async (req, res) => {
-        const { id, timestamp, title, body, author, category } = req.body;
+        const { timestamp, title, body, id, category } = req.body;
+        const id = Date.now();
         const post = new Post({
-            timestamp,
+            createdTime: timestamp,
+            editedTime: null,
             title,
             body,
             voteScore: 0,
+            comments: [],
         });
+        post.author = id;
+        post.category = category;
+
         const response = await post.save();
+
         res.send(response);
     });
-    // get a postF
+    // get a post by id
     app.get('/posts/:postId', async (req, res) => {
         const { params: { postId } } = req;
         const post = await Post.findById(postId);
         res.send(post);
     });
-    // vote a post
+    // vote a post by id
     app.post('/posts/:postId', async (req, res) => {
         const { params: { postId }, body: { option } } = req;
         const change = option === 'upvote' ? 1 : -1;
@@ -35,14 +45,14 @@ module.exports = app => {
         const updatedPost = await Post.findById(postId);
         res.send(updatedPost);
     })
-    // edit a post
+    // edit a post by id
     app.put('/posts/:postId', async (req, res) => {
         const { params: { postId }, body: { title, body } } = req;
         await Post.findByIdAndUpdate(postId, { $set: { title, body } });
         const updatedPost = await Post.findById(postId);
         res.send(updatedPost);
     })
-    // delete a post
+    // delete a post by id
     app.delete('/posts/:postId', async (req, res) => {
         const { params: { postId } } = req;
         const deletedPost = Post.findByIdAndRemove(postId);
