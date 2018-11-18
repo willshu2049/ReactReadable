ad5a1d14606826bdddead874bd5c88348a0998ab
Will Shu <will@Wills-iMac.local>
2018-11-18 15:55:32 +0800

    dev modified category routes

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -2,8 +2,18 @@ const mongoose = require('mongoose');
 const Category = mongoose.model('category');
 
 module.exports = app => {
-    app.get('/categories', async (req, res) => {
-        const categories = Category.find({});
+    app.post('/categories', async (req, res) => {
+        const { body:{page, count} } = req;
+        const categories = await Category.find({})
+            .skip(Category.count() - count*page)
+            .limit(count);
         res.send(categories);
+    });
+
+    app.post('/categories/add', async (req, res)=> {
+        const { body:{ name } } = req;
+        const category = new Category({name});
+        const response = await category.save();
+        res.send(response);
     })
 }
